afbf5b6cd8ae30267aa30a9cde68ba1cbf9237f1
Ondřej Musil <o@superface.ai>
2022-03-28 11:07:04 +0200

    feat: prepare perform to get security

--- index.ts
+++ index.ts
@@ -1,9 +1,22 @@
 import {
+  HttpScheme,
+  isApiKeySecurityValues,
+  isBasicAuthSecurityValues,
+  isBearerTokenSecurityValues,
+  isDigestSecurityValues,
   MapDocumentNode,
   ProfileDocumentNode,
   ProviderJson,
+  SecurityScheme,
+  SecurityType,
+  SecurityValues,
 } from '@superfaceai/ast';
 import { BoundProfileProvider } from '@superfaceai/one-sdk';
+import {
+  invalidSecurityValuesError,
+  securityNotFoundError,
+} from '@superfaceai/one-sdk/dist/internal/errors.helpers';
+import { SecurityConfiguration } from '@superfaceai/one-sdk/dist/internal/interpreter/http';
 import { ServiceSelector } from '@superfaceai/one-sdk/dist/lib/services';
 import { parseMap, parseProfile, Source } from '@superfaceai/parser';
 import { readFile } from 'fs/promises';
@@ -26,12 +39,96 @@ async function loadUsecase() {
   };
 }
 
+export function resolveSecurityConfiguration(
+  schemes: SecurityScheme[],
+  values: SecurityValues[],
+  providerName: string
+): SecurityConfiguration[] {
+  const result: SecurityConfiguration[] = [];
+
+  for (const vals of values) {
+    const scheme = schemes.find((scheme) => scheme.id === vals.id);
+    if (scheme === undefined) {
+      const definedSchemes = schemes.map((s) => s.id);
+      throw securityNotFoundError(providerName, definedSchemes, vals);
+    }
+
+    const invalidSchemeValuesErrorBuilder = (
+      scheme: SecurityScheme,
+      values: SecurityValues,
+      requiredKeys: [string, ...string[]]
+    ) => {
+      const valueKeys = Object.keys(values).filter((k) => k !== 'id');
+
+      return invalidSecurityValuesError(
+        providerName,
+        scheme.type,
+        scheme.id,
+        valueKeys,
+        requiredKeys
+      );
+    };
+
+    if (scheme.type === SecurityType.APIKEY) {
+      if (!isApiKeySecurityValues(vals)) {
+        throw invalidSchemeValuesErrorBuilder(scheme, vals, ['apikey']);
+      }
+
+      result.push({
+        ...scheme,
+        ...vals,
+      });
+    } else {
+      switch (scheme.scheme) {
+        case HttpScheme.BASIC:
+          if (!isBasicAuthSecurityValues(vals)) {
+            throw invalidSchemeValuesErrorBuilder(scheme, vals, [
+              'username',
+              'password',
+            ]);
+          }
+
+          result.push({
+            ...scheme,
+            ...vals,
+          });
+          break;
+
+        case HttpScheme.BEARER:
+          if (!isBearerTokenSecurityValues(vals)) {
+            throw invalidSchemeValuesErrorBuilder(scheme, vals, ['token']);
+          }
+
+          result.push({
+            ...scheme,
+            ...vals,
+          });
+          break;
+
+        case HttpScheme.DIGEST:
+          if (!isDigestSecurityValues(vals)) {
+            throw invalidSchemeValuesErrorBuilder(scheme, vals, ['digest']);
+          }
+
+          result.push({
+            ...scheme,
+            ...vals,
+          });
+          break;
+      }
+    }
+  }
+
+  return result;
+}
+
 export type PerformOpts<TInput> = {
   profileAst: ProfileDocumentNode;
   mapAst: MapDocumentNode;
   provider: ProviderJson;
   usecase: string;
   input?: TInput;
+  security?: SecurityValues[];
   parameters?: Record<string, string>;
 };
 
@@ -41,6 +138,7 @@ async function perform({
   provider,
   usecase,
   input,
+  security,
   parameters,
 }: PerformOpts<any>) {
   const boundProvider = new BoundProfileProvider(
@@ -49,7 +147,11 @@ async function perform({
     provider.name,
     {
       services: new ServiceSelector(provider.services, provider.defaultService),
-      security: [], // TODO: I need to uncover how this is created. Too many layers. And I am worried it will be hard to reuse as it is private https://github.com/superfaceai/one-sdk-js/blob/c6ed48b6e58e02cde11b0baa6615b5aa4c4f15ba/src/client/profile-provider.ts#L634
+      security: resolveSecurityConfiguration(
+        provider.securitySchemes ?? [],
+        security ?? [],
+        provider.name
+      ),
     }
   );
 
@@ -62,6 +164,12 @@ async function main() {
   const profileAst = await parseProfile(new Source(usecase.profile));
   const mapAst = await parseMap(new Source(usecase.map));
   const provider = JSON.parse(usecase.provider);
+
+  // TODO: create security from env file
+  const security = [];
+
+  // TODO: gather integration prameters from env file
+  const parameters = {};
 
   try {
     const result = await perform({
